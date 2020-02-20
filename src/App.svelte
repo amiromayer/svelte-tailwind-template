c80fe2b0b188d69067bec3456640dc6f9d635d60
Amir <amiromayer@gmail.com>
2020-02-20 14:25:58 +0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Tailwindcss from "./Tailwindcss.svelte";
+  export let name;
+</script>
+
+<style>
+
+</style>
+
+<Tailwindcss />
+<h1 class="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+  Hello {name}!
+</h1>
